d5d1b66f796ba49460c22856c70cc6b714005ced
adlathifaifaa <wandalathifa25@gmail.com>
2026-04-22 18:59:29 +0700

    Upload tugas CI4

--- app/Config/Filters.php
+++ app/Config/Filters.php
@@ -13,6 +13,7 @@ use CodeIgniter\Filters\PageCache;
 use CodeIgniter\Filters\PerformanceMetrics;
 use CodeIgniter\Filters\SecureHeaders;
 
+
 class Filters extends BaseFilters
 {
     /**
@@ -34,6 +35,7 @@ class Filters extends BaseFilters
         'forcehttps'    => ForceHTTPS::class,
         'pagecache'     => PageCache::class,
         'performance'   => PerformanceMetrics::class,
+        'auth' => \App\Filters\Auth::class
     ];
 
     /**
